6d6e7934ad24054fcca3f4a1477aba72f9b7e6f9
Janice <janice@baileywx.com>
2020-09-14 06:46:06 -0400

    Create TranslateDemo.java

---
+++ TranslateDemo.java
@@ -0,0 +1,22 @@
+/**
+ * Creates a rectangle and sets a window to size and shape of the rectangle
+ * Author:
+ * Date:
+ */
+
+import java.awt.Rectangle;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+public class TranslateDemo
+{
+   public static void main(String[] args)
+   {
+      // Construct a frame and show it
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+      // Your work goes here: Construct a rectangle and set the frame bounds
+      JOptionPane.showMessageDialog(frame, "Click OK to continue");
+      // Your work goes here: Move the rectangle and set the frame bounds again
+   }
+}
